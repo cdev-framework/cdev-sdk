e04cb68312107f205a8f6963e417ca9a7e2d1fb9
Daniel Sanchez <arturo97@live.unc.edu>
2021-09-13 11:01:39 -0400

    add simple queue creation

---
+++ src/cdev/mapper/backend/simple/queue_deployer.py
@@ -0,0 +1,71 @@
+from enum import Enum
+from types import new_class
+from typing import Dict, List
+
+from cdev.models import Resource_State_Difference, Action_Type
+from cdev.utils import hasher, logger
+from cdev.resources.simple import queue as simple_queue
+from cdev.backend import cloud_mapper_manager as cdev_cloud_mapper
+from ..aws import aws_client as raw_aws_client
+
+log = logger.get_cdev_logger(__name__)
+RUUID = simple_queue.RUUID
+
+
+def _create_simple_queue(identifier: str, resource: simple_queue.simple_queue_model) -> bool:
+    attributes = {}
+
+    if resource.fifo:
+        attributes.update({
+            "FifoQueue": str(resource.fifo)
+        })
+
+    
+    rv = raw_aws_client.run_client_function("sqs", "create_queue", {
+        "QueueName": resource.queue_name if not resource.fifo else f"{resource.queue_name}.fifo",
+        "Attributes": attributes
+    })
+
+    output_info = {
+        "queue_name": resource.queue_name,
+        "cdev_name": resource.name,
+        "ruuid": RUUID,
+        "queue_url": rv.get("QueueUrl")
+    }
+
+    # rv does not contain the arn of the queue so we need a second call
+
+    rv = raw_aws_client.run_client_function("sqs", "get_queue_attributes", {
+        "QueueUrl": output_info.get("queue_url"),
+        "AttributeNames": ['QueueArn']
+    })
+
+    output_info.update({
+        "cloud_id": rv.get("Attributes").get("QueueArn"),
+        "arn": rv.get("Attributes").get("QueueArn")
+    })
+
+    cdev_cloud_mapper.add_identifier(identifier),
+    cdev_cloud_mapper.update_output_value(identifier, output_info)
+    return True
+
+
+def _update_simple_queue(previous_resource: simple_queue.simple_queue_model, new_resource: simple_queue.simple_queue_model) -> bool:
+    pass
+
+
+def _remove_simple_queue(identifier: str, resource: simple_queue.simple_queue_model) -> bool:
+    pass
+
+def handle_simple_queue_deployment(resource_diff: Resource_State_Difference) -> bool:
+    try:
+        if resource_diff.action_type == Action_Type.CREATE:
+            return _create_simple_queue(resource_diff.new_resource.hash, resource_diff.new_resource)
+        elif resource_diff.action_type == Action_Type.UPDATE_IDENTITY:
+            return _update_simple_queue(resource_diff.previous_resource, resource_diff.new_resource)
+        elif resource_diff.action_type == Action_Type.DELETE:
+            return _remove_simple_queue(resource_diff.previous_resource.hash, resource_diff.previous_resource)
+
+    except Exception as e:
+        print(e)
+        raise Exception("COULD NOT DEPLOY")
--- src/cdev/mapper/cloudmapper.py
+++ src/cdev/mapper/cloudmapper.py
@@ -8,7 +8,7 @@ from cdev.backend import cloud_mapper_manager as cdev_cloud_mapper
 
 from .backend.aws import aws_lambda, dynamodb, iam, s3, sqs, apigatewayv2, apigateway
 
-from .backend.simple import api_deployer, lambda_deployer, dynamodb_deployer, bucket_deployer
+from .backend.simple import api_deployer, lambda_deployer, dynamodb_deployer, bucket_deployer, queue_deployer
 
 
 class DefaultMapper(CloudMapper):
@@ -121,6 +121,7 @@ RESOURCE_TO_HANDLER_FUNCTION = {
     "cdev::simple::api": api_deployer.handle_simple_api_deployment,
     "cdev::simple::lambda_function": lambda_deployer.handle_simple_lambda_function_deployment,
     "cdev::simple::table": dynamodb_deployer.handle_simple_table_deployment,
-    "cdev::simple::bucket": bucket_deployer.handle_simple_bucket_deployment
+    "cdev::simple::bucket": bucket_deployer.handle_simple_bucket_deployment,
+    "cdev::simple::queue": queue_deployer.handle_simple_queue_deployment
 }
 
