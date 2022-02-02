bd7dc5425a7104e0929c79396ab51e7b35af1bc4
Daniel Sanchez <arturo97@live.unc.edu>
2022-02-01 20:01:45 -0500

    add tests for hashers

---
+++ tests/core/utils/test_hasher.py
@@ -0,0 +1,31 @@
+from core.utils import hasher
+
+
+def test_hash_list():
+    d = [
+        "1",
+        "2"
+        "3",
+        "4"
+    ]
+
+    assert hasher.hash_list(d) == hasher.hash_list(d)
+    d_reversed = d.copy()
+    d_reversed.reverse()
+
+    assert not hasher.hash_list(d_reversed) == hasher.hash_list(d)
+    assert not hasher.hash_list(d[:-1]) == hasher.hash_list(d)
+    assert not hasher.hash_list(["random"]) == hasher.hash_list(d)
+
+def test_hash_string():
+    d = "12"
+
+    assert hasher.hash_string(d) == hasher.hash_string(d)
+    d_reversed = d[::-1]
+
+    assert not hasher.hash_string(d_reversed) == hasher.hash_string(d)
+    assert not hasher.hash_string(d[:-1]) == hasher.hash_string(d)
+    assert not hasher.hash_string("random") == hasher.hash_string(d)
+
+def test_hash_file():
+    pass
