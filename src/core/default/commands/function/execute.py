021d1db846de8e12f0c33445ee60b061bfa91424
Daniel Sanchez <arturo97@live.unc.edu>
2022-02-14 15:59:25 -0500

    add ability to execute functions with events from command line

--- src/core/default/commands/function/execute.py
+++ src/core/default/commands/function/execute.py
@@ -1,7 +1,9 @@
 
 from argparse import ArgumentParser
-
+import json
+import os
 from typing import List, Dict
+
 from boto3 import client
 
 from core.constructs.commands import BaseCommand, OutputWrapper
@@ -23,21 +25,55 @@ class execute(BaseCommand):
 
     def add_arguments(self, parser: ArgumentParser):
         parser.add_argument(
-            "function_name", type=str, help="The function that you want to watch. Name must include component name. ex: comp1.demo_function"
+            "function_id", 
+            type=str, 
+            help="The id of the function to execute."
         )
         parser.add_argument(
             "--event",
-            action="store_true",
-            help="watch the logs. If this flag is passed, only --start_time flag is read",
+            type=str,
+            help="File (json) location of event object to provide as input to the function. Can not be used with '--event-data` flag.",
         )
         parser.add_argument(
-            "--context", action="store_true", help="show the tail of the logs"
+            "--event-data",
+            type=str,
+            help="Raw string form of event object to provide as input to the function. Can not be used with '--event' flag.",
         )
+        
        
 
     def command(self, *args, **kwargs):
 
-        full_function_name: str = kwargs.get("function_name")
+        full_function_name: str = kwargs.get("function_id")
+
+        event_file_location: str = kwargs.get("event")
+        event_raw_data: str = kwargs.get("event_data")
+
+        if event_file_location and event_raw_data:
+            raise Exception("Can not provide both '--event-data' and '--event'")
+
+        event_data = {}
+
+        if event_file_location:
+            if not os.path.isfile(event_file_location):
+                raise Exception(f"{event_file_location} is not a valid file location")
+
+            with open(event_file_location) as fh:
+                try:
+                    event_data = json.load(fh)
+                except Exception as e:
+                    print(e)
+                    raise Exception(f'Could not load {event_file_location} as json')
+
+                
+        if event_raw_data:
+            try:
+                event_data = json.loads(event_raw_data)
+            except Exception as e:
+                print(e)
+                raise Exception(f'Could not load {event_raw_data} as json')
+
+
         component_name = full_function_name.split('.')[0]
         function_name = full_function_name.split('.')[1]
 
@@ -49,6 +85,7 @@ class execute(BaseCommand):
         response = lambda_client.invoke(
             FunctionName=cloud_name,
             InvocationType='RequestResponse',
+            Payload=json.dumps(event_data)
         )
 
 
