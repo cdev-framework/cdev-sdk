4b9a4e3bb345b7aabf04897b75851da9e3bd43dc
Daniel Sanchez <arturo97@live.unc.edu>
2022-02-17 14:52:17 -0500

    .

--- src/cdev/commands/destroy.py
+++ src/cdev/commands/destroy.py
@@ -1,8 +1,10 @@
 from cdev.constructs.project import Project
 from cdev.default.output_manager import CdevOutputManager
 from cdev.cli.logger import set_global_logger_from_cli
+from rich.prompt import Confirm
 
-from core.commands.deploy_differences import execute_deployment
+
+from core.constructs.workspace import Workspace_State
 
 def destroy_command_cli(args):
     config = args[0]
@@ -15,5 +17,29 @@ def destroy_command(args):
     output_manager = CdevOutputManager()
     myProject = Project.instance()
 
-    print(f"Destory")
+    ws = myProject.get_current_environment().get_workspace()
 
+
+    ws.set_state(Workspace_State.EXECUTING_FRONTEND)
+
+    diff_previous_component_names = [x.name for x in ws.get_backend().get_resource_state(ws.get_resource_state_uuid()).components]
+
+
+    differences = ws.create_state_differences([], diff_previous_component_names)
+
+    if any(x for x in differences):
+        output_manager.print_state_differences(differences)
+
+
+    
+
+    print("")
+    do_deployment = Confirm.ask("Are you sure you want to Destroy the environment?")
+
+    if not do_deployment:
+        return
+
+    differences_structured = ws.sort_differences(differences)
+    ws.set_state(Workspace_State.EXECUTING_BACKEND)
+
+    ws.deploy_differences(differences_structured)
