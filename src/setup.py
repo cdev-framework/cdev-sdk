5bfb3443bd0184920cf87f33fe71dd4693303483
Daniel Sanchez <arturo97@live.unc.edu>
2022-03-13 14:32:25 -0400

    update requirements to include aurora-data-api dependency

--- src/setup.py
+++ src/setup.py
@@ -23,6 +23,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
+        "aurora_data_api",
         "boto3",
         "botocore",
         "certifi",
