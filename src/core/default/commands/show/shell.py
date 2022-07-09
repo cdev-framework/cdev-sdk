516de83f2a5abc332925255570e04ea1f3e5d4b9
Cleober De Carvalho Alonso Fernandes <cleoberf@gmail.com>
2022-07-09 14:33:38 -0300

    viewing contents of a deployed Serverless Function

---
+++ src/core/default/commands/show/shell.py
@@ -0,0 +1,110 @@
+import cmd
+import readline
+import os
+
+from argparse import ArgumentParser
+from core.constructs.workspace import Workspace
+from typing import List, Tuple
+
+import aurora_data_api
+from rich.console import Console
+from rich.table import Table
+
+from core.constructs.commands import BaseCommand
+from core.default.commands.relationaldb.utils import get_db_info_from_cdev_name
+
+from textual.app import App
+from textual.reactive import Reactive
+from textual.widgets import Footer, Placeholder
+from textual.widget import Widget
+from textual.widget import Panel
+
+
+import zipfile
+
+import os
+from rich.console import RenderableType
+
+from rich.syntax import Syntax
+from rich.traceback import Traceback
+
+from textual.app import App
+from textual.widgets import Header, Footer, FileClick, ScrollView, DirectoryTree
+
+
+class shell(BaseCommand):
+    help = """
+        Open an interactive shell to a relational db.
+    """
+
+    # def add_arguments(self, parser: ArgumentParser) -> None:
+        # parser.add_argument(
+        #     "resource",
+        #     type=str,
+        #     help="The database to execute on. Name must include component name. ex: comp1.myDb",
+        # )
+
+    def command(self, *args, **kwargs) -> None:
+        full_file_path = '/Users/cleoberf/Documents/cdev/resources_hello_world_function.zip'
+        file_path = '/Users/cleoberf/Documents/cdev/unzip/'
+        with zipfile.ZipFile(full_file_path, 'r') as zip_ref:
+            zip_ref.extractall(file_path)
+
+        class MyApp(App):
+            """An example of a very simple Textual App"""
+
+            async def on_load(self) -> None:
+                """Sent before going in to application mode."""
+
+                # Bind our basic keys
+                await self.bind("b", "view.toggle('sidebar')", "Toggle sidebar")
+                await self.bind("q", "quit", "Quit")
+
+                # Get path to show
+                try:
+                    self.path = file_path
+                except IndexError:
+                    self.path = os.path.abspath(
+                        os.path.join(os.path.basename(__file__), "../../")
+                    )
+
+            async def on_mount(self) -> None:
+                """Call after terminal goes in to application mode"""
+
+                # Create our widgets
+                # In this a scroll view for the code and a directory tree
+                self.body = ScrollView()
+                self.directory = DirectoryTree(self.path, "Files")
+
+                # Dock our widgets
+                await self.view.dock(Header(), edge="top")
+                await self.view.dock(Footer(), edge="bottom")
+
+                # Note the directory is also in a scroll view
+                await self.view.dock(
+                    ScrollView(self.directory), edge="left", size=48, name="sidebar"
+                )
+                await self.view.dock(self.body, edge="top")
+
+            async def handle_file_click(self, message: FileClick) -> None:
+                """A message sent by the directory tree when a file is clicked."""
+
+                syntax: RenderableType
+                try:
+                    # Construct a Syntax object for the path in the message
+                    syntax = Syntax.from_path(
+                        message.path,
+                        line_numbers=True,
+                        word_wrap=True,
+                        indent_guides=True,
+                        theme="monokai",
+                    )
+                except Exception:
+                    # Possibly a binary file
+                    # For demonstration purposes we will show the traceback
+                    syntax = Traceback(theme="monokai", width=None, show_locals=True)
+                self.app.sub_title = os.path.basename(message.path)
+                await self.body.update(syntax)
+
+        # Run our app class
+        MyApp.run(title="Code Viewer", log="textual.log")
