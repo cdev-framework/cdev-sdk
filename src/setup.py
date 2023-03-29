e4d5fae895426cb9e9b715f24fa0d9a2e51442d6
Daniel <15sanchd@gmail.com>
2023-03-29 15:41:07 -0400

    bump version

--- src/setup.py
+++ src/setup.py
@@ -23,7 +23,7 @@ def _get_readme_contents() -> str:
 
 setuptools.setup(
     name="cdev",
-    version="0.0.28",
+    version="0.0.29",
     scripts=["./cdev/scripts/cdev", "./core/scripts/cdev_core"],
     description="CLI for cdev sdk",
     long_description=_get_readme_contents(),
